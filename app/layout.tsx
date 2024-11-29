bfa12c5f0717a1309f5cdc954013d277eea2c663
Anthony Ostia <meximanianace@gmail.com>
2024-11-29 10:36:27 -0800

    refactor: fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,25 +3,18 @@
 import { FC, ReactNode } from "react";
 import "./globals.css";
 
-// interface RootLayoutProps {
-//   children: ReactNode;
-// }
+interface RootLayoutProps {
+  children: ReactNode;
+}
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { AnimatePresence, motion } from "framer-motion";
 
-const RootLayout = ({ children, modal }) => {
+const RootLayout:FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <body>
         <AnimatePresence>{children}</AnimatePresence>
-        <AnimatePresence
-          mode="wait"
-          initial={false}
-          onExitComplete={() => null}
-        >
-          {modal}
-        </AnimatePresence>
       </body>
     </html>
   );
--- app/pageWrapper.tsx
+++ app/pageWrapper.tsx
@@ -10,14 +10,14 @@ interface PageWrapperProps {
 const PageWrapper: FC<PageWrapperProps> = ({ children }) => {
   return (
     <AnimatePresence>
-      {/* <motion.div
+      <motion.div
         initial={{ opacity: 0, y: 25 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 25 }}
         transition={{ delay: 0.5 }}
-      > */}
+      >
         {children}
-      {/* </motion.div> */}
+      </motion.div>
     </AnimatePresence>
   );
 };
