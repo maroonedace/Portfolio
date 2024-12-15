5766c09df2edb5d31d959f523d6e3841de0c689f
Anthony Ostia <meximanianace@gmail.com>
2024-12-15 15:19:54 -0800

    feat: add contact items

--- app/components/body/contact/contact.tsx
+++ app/components/body/contact/contact.tsx
@@ -1,7 +1,27 @@
-const Contact = () => {
-    return (
-    <div></div>
-    )
-}
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 
-export default Contact
+import mailSvg from "../../../assets/contact/mail.svg";
+
+const Contact = () => {
+  return (
+    <div className="p-8 flex flex-col gap-2 items-center">
+      <h2 className="text-white">Contact Me</h2>
+      <motion.div
+        initial={{ scale: 1 }}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 1 }}
+      >
+        <Link
+          href="mailto:aostia@msn.com?subject=Porfolio Contact Form"
+          className="flex items-center"
+        >
+          <Image width="60" height="60" src={mailSvg} alt="Mail" />
+        </Link>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Contact;
