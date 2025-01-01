80e945c5c280f9465c1beaa1ee99ba45a11942bb
Anthony Ostia <meximanianace@gmail.com>
2024-12-31 18:22:54 -0800

    refactor: add animation for skills

--- app/components/body/skills/skills.tsx
+++ app/components/body/skills/skills.tsx
@@ -14,13 +14,29 @@ const Skills: FC<SkillsProps> = ({ componentRef }) => {
         <div className="flex flex-col gap-4 bg-gray-700 rounded-2xl h-[85vh] p-8">
           <div className="flex flex-col text-center gap-8 h-full justify-center items-center">
             <div className="flex flex-col gap-4">
-              <h3 className="sm:text-xl md:text-2xl lg:text-3xl">Languages</h3>
+              <motion.h3
+                initial={{ scale: 0, opacity: 0 }}
+                whileInView={{ scale: 1, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ delay: 0.3 }}
+                className="sm:text-xl md:text-2xl lg:text-3xl"
+              >
+                Languages
+              </motion.h3>
               <div className="flex flex-row gap-4 flex-wrap justify-center items-center">
                 <SkillList listOfSkills={languageSkillList} />
               </div>
             </div>
             <div className="flex flex-col gap-4">
-              <h3 className="sm:text-xl md:text-2xl lg:text-3xl">Technologies</h3>
+              <motion.h3
+                initial={{ scale: 0, opacity: 0 }}
+                whileInView={{ scale: 1, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ delay: 0.3 }}
+                className="sm:text-xl md:text-2xl lg:text-3xl"
+              >
+                Technologies
+              </motion.h3>
               <div className="flex flex-row gap-4 flex-wrap justify-center items-center">
                 <SkillList listOfSkills={technologySkillList} />
               </div>
