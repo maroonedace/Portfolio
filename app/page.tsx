261c38449a0874613debe1a47e3b5c21c395dc3d
Anthony Ostia <meximanianace@gmail.com>
2025-07-06 14:57:19 -0700

    refactor: add image

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ import Skills from "./components/body/skills/skills";
 import Project from "./components/body/project/project";
 
 import type { Metadata } from 'next'
- 
+
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 export const metadata: Metadata = {
   title: "Anthony Ostia | Software Engineer",
   description: "Hi, I'm Anthony Ostia. Always looking for new opportunities to grow and engage in insightful conversations.",
@@ -16,12 +19,12 @@ export const metadata: Metadata = {
     description: "Hi, I'm Anthony Ostia. Always looking for new opportunities to grow and engage in insightful conversations.",
     images: [
       {
-        url: "https://your-domain.com/preview.jpg",
+        url: `${baseUrl}/preview.jpg`,
         width: 1200,
         height: 630,
       },
     ],
-    url: "https://anthony-ostia.vercel.app",
+    url: baseUrl,
     type: "website",
   },
 }
