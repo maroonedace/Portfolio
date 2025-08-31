b33f39cb34675b3b671b1196052f649dff027884
Anthony Ostia <meximanianace@gmail.com>
2025-08-31 09:49:33 -0700

    fix: maruqee design

--- app/components/body/skills/skillsShowcase/marquee.tsx
+++ app/components/body/skills/skillsShowcase/marquee.tsx
@@ -7,10 +7,7 @@ type MarqueeProps = {
   direction: "left" | "right";
 };
 
-const Marquee = ({
-  children,
-  direction,
-}: MarqueeProps) => {
+const Marquee = ({ children, direction }: MarqueeProps) => {
   const a1 =
     direction === "left" ? "animate-marqueeLeft" : "animate-marqueeRight";
   const a2 =
@@ -19,35 +16,23 @@ const Marquee = ({
   return (
     <div className="relative overflow-hidden">
       <div
-        className="relative"
-        style={{
-          WebkitMaskImage:
-            "linear-gradient(to right, transparent, black 24px, black calc(100% - 24px), transparent)",
-          maskImage:
-            "linear-gradient(to right, transparent, black 24px, black calc(100% - 24px), transparent)",
-          WebkitMaskRepeat: "no-repeat",
-          maskRepeat: "no-repeat",
-        }}
+        className={clsx(
+          a1,
+          "inline-flex items-center gap-2 whitespace-nowrap w-max will-change-transform"
+        )}
       >
-        <div
-          className={clsx(
-            a1,
-            "inline-flex items-center gap-2 whitespace-nowrap w-max will-change-transform"
-          )}
-        >
-          {children}
-        </div>
-        <div
-          className={clsx(
-            a2,
-            "absolute top-0 inline-flex items-center gap-2 whitespace-nowrap w-max will-change-transform"
-          )}
-        >
-          {children}
-        </div>
+        {children}
+      </div>
+      <div
+        className={clsx(
+          a2,
+          "absolute top-0 inline-flex items-center gap-2 whitespace-nowrap w-max will-change-transform left-0"
+        )}
+      >
+        {children}
       </div>
     </div>
   );
-}
+};
 
-export default Marquee
+export default Marquee;
