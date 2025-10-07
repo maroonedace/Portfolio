eb588e0c931ce35b886648b55474246ef460e5b8
Anthony Ostia <44576266+maroonedace@users.noreply.github.com>
2025-10-06 23:02:03 -0700

    refactor: reusable contact info

--- app/components/contact/page.tsx
+++ app/components/contact/page.tsx
@@ -3,54 +3,39 @@
 import Link from "next/link";
 import Image from "next/image";
 
+const contactInfo = [
+  {
+    href: "mailto:aostia815@gmail.com",
+    src: "/images/contact/mail.svg",
+    alt: "Mail",
+  },
+  {
+    href: "https://github.com/maroonedace",
+    src: "/images/skills/github.svg",
+    alt: "Github",
+  },
+  {
+    href: "https://linkedin.com/in/aostia",
+    src: "/images/contact/linkedin.svg",
+    alt: "LinkedIn",
+  },
+];
+
 const Contact = () => {
   return (
     <div className="flex justify-center items-center p-4" id="contact">
       <div className="bg-zinc-800 flex flex-col gap-4 items-center w-full sm:w-2/3 lg:w-1/2 py-4 rounded-2xl">
         <h3 className="text-2xl font-semibold uppercase">Let's Connect</h3>
         <div className="flex gap-8">
-          <Link
-            href="mailto:aostia815@gmail.com"
-            tabIndex={0}
-            className="bg-white focus-ring p-2 rounded-full flex hover:scale-110 active:scale-95 transition motion-reduce:transform-none"
-          >
-            <Image
-              src="/images/contact/mail.svg"
-              alt="Mail"
-              width={48}
-              height={48}
-            />
-          </Link>
-          <Link
-            href="https://github.com/maroonedace"
-            target="_blank"
-            rel="noopener noreferrer"
-            tabIndex={0}
-            aria-label="GitHub"
-            className="bg-white focus-ring p-2 rounded-full flex hover:scale-110 active:scale-95 transition motion-reduce:transform-none"
-          >
-            <Image
-              src="/images/skills/github.svg"
-              alt="Github Logo"
-              width={48}
-              height={48}
-            />
-          </Link>
-          <Link
-            href="https://linkedin.com/in/aostia"
-            target="_blank"
-            rel="noopener noreferrer"
-            tabIndex={0}
-            aria-label="LinkedIn"
-            className="bg-white focus-ring p-2 rounded-full flex hover:scale-110 active:scale-95 transition motion-reduce:transform-none"
-          >
-            <Image
-              src="/images/contact/linkedin.svg"
-              alt="LinkedIn Logo"
-              width={48}
-              height={48}
-            />
-          </Link>
+          {contactInfo.map((info) => (
+            <Link
+              href={info.href}
+              tabIndex={0}
+              className="bg-white focus-ring p-2 rounded-full flex hover:scale-110 active:scale-95 transition motion-reduce:transform-none"
+            >
+              <Image src={info.src} alt={info.alt} width={48} height={48} />
+            </Link>
+          ))}
         </div>
       </div>
     </div>
