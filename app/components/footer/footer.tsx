a9597c84d7987ead00a6187a81a022f42e6f3aac
Anthony Ostia <44576266+maroonedace@users.noreply.github.com>
2025-10-07 21:53:12 -0700

    refactor: footer component

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer className="border-t border-zinc-700 bg-zinc-700 p-4">
+    <footer className=" bg-primary p-4">
       <div className="flex flex-col items-start sm:items-center md:items-end">
         <p className="text-sm text-right">
           © {new Date().getFullYear()} Anthony Ostia
