a9bf9dc42af166307f533c5c52f5997cbac22498
Anthony Ostia <44576266+maroonedace@users.noreply.github.com>
2025-10-08 22:43:43 -0700

    refactor: clean github logo

--- app/components/contact/contact.tsx
+++ app/components/contact/contact.tsx
@@ -26,17 +26,22 @@ const Contact = () => {
       <div className="bg-primary flex flex-col gap-4 items-center w-full sm:w-2/3 lg:w-1/2 py-4 rounded-2xl">
         <h4 className="uppercase">Let's Connect</h4>
         <div className="flex gap-8">
-          {contactInfo.map((info) => (
-            <Link
-              href={info.href}
-              key={info.alt}
-              tabIndex={0}
-              className="bg-secondary text-primary focus-ring p-2 rounded-full flex items-center md:w-40 justify-center gap-2 hover:scale-110 active:scale-95 transition"
-            >
-              <i className={`ph-fill ${info.src} text-[48px]`} />
-              <span className="hidden md:inline font-medium">{info.alt}</span>
-            </Link>
-          ))}
+          {contactInfo.map((info) => {
+            const isGithub = info.alt === "Github";
+            return (
+              <Link
+                href={info.href}
+                key={info.alt}
+                tabIndex={0}
+                className="bg-secondary text-primary focus-ring p-2 rounded-full flex items-center md:w-40 justify-center gap-2 hover:scale-110 active:scale-95 transition"
+              >
+                <div className={`${isGithub ? "p-2 rounded-full bg-primary text-secondary text-3xl" : "text-5xl"} w-12 h-12 flex items-center justify-center`}>
+                  <i className={`ph-fill ${info.src}`} />
+                </div>
+                <span className="hidden md:inline font-medium">{info.alt}</span>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
